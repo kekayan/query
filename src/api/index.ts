455d7713e06d35dad251897c96cac9678a9235e4
kekayan <kekayannanthakumar@gmail.com>
2025-08-03 18:46:04 +1200

    remove unused SPARC API types and exports

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-export { SparcApi, createSparcApi } from './sparc';
-
-export type {
-  SparcSearchResponse,
-  SparcDatasetHit,
-  SparcDataset,
-  SparcSearchQuery,
-  SparcApiConfig
-} from '../types/sparc';
